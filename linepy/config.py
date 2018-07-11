0dc3595cccf9c94c79e4756a6af63438a8504f63
rafihadi04 <32604312+rafihadi04@users.noreply.github.com>
2018-07-11 18:11:19 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -29,7 +29,7 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '2.1.15'
+    APP_VER     = '1.4.17'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'BOLPC'
     SYSTEM_VER  = '11.2.5'
